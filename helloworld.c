6f1f8ed3ba39aada5f32973d916860015f882934
EequalMC2 <muratonr@yahoo.com.tr>
2020-05-10 15:38:14 +0300

    added foo function

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 
+void foo();
+
 int main()
 {
 
 printf("hello world\n");
-
+foo();
 return 0;
 
 }
+
+void foo(){
+
+   for(int i = 0; i < 5; i++)
+	printf("%d",i);
+   printf("\n");
+}
+
